95c0b9bf5397c512fa51f31f7ea2b38d0d7f9ebc
williamszk <wllmszk@gmail.com>
2023-06-08 21:44:26 -0300

    flask study

--- udemy_restapi_teclado/230508/notes01.sh
+++ udemy_restapi_teclado/230508/notes01.sh
@@ -1,6 +1,6 @@
 
 python -m venv .venv
-
+source .venv/bin/activate
 pip install black mypy
 pip install flask 
 
@@ -8,4 +8,14 @@ touch app.py
 
 # how to run the flask app
 flask run --debug
+
+touch Dockerfile
+
+docker build -t williamszk/rest-api-flask-python .
+
+docker run --rm -p 5000:5000 --name rest-api-flask-python williamszk/rest-api-flask-python
+
+docker kill rest-api-flask-python
+docker rm rest-api-flask-python
+
 
