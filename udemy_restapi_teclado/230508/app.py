50b967f8892dcac1ca76ef003e3c510c4f9427fa
williamszk <wllmszk@gmail.com>
2023-06-10 01:59:56 +0000

    flask course udemy

--- udemy_restapi_teclado/230508/app.py
+++ udemy_restapi_teclado/230508/app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 
 from db import stores, items
 
@@ -14,6 +15,20 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+
+    # ensure that name field is included
+    # (i.e. name must be provided)
+    if "name" not in store_data:
+        abort(
+            400, message="Bad request. Ensure 'name' is included in the JSON payload."
+        )
+
+    # ensure the store is not duplicated
+    # (i.e. name must be unique)
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
+
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -25,9 +40,30 @@ def create_item():
     item_data = request.get_json()
     # The id of the store should come inside the json payload
 
+    # This is where the checking for the incoming payload is checked
+    # We still need to check if price is a number, store_id and name are strings
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload.",
+        )
+
+    # check to not insert an item that already exists
+    for item in items.values():
+        if (
+            item["name"] == item_data["name"]
+            and item["store_id"] == item_data["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
+
     # Check if the store_id in the json payload already exists in the stores data structure
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
+        # abort will exit and will auto document this error message
     # "name"
     # "price"
     # "store_id"
@@ -43,11 +79,12 @@ def get_all_items():
     return {"items": list(items.values())}
 
 
+@app.get("/store/<string:store_id>")
 def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
 
 
 @app.get("/item/<string:item_id>")
@@ -55,4 +92,4 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Item not found")
